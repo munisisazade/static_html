2167bf8c7fe9858b73c6be497a8b9c1f8459ceeb
munisisazade <munisisazade@gmail.com>
2019-02-08 17:56:40 +0400

    Update

--- ismayil/settings.py
+++ ismayil/settings.py
@@ -212,34 +212,12 @@ SITE_ID = 1
 # Celery backend result
 CELERY_RESULT_BACKEND = 'django-db'
 
-if PROD:  # For production docker use
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ.get('POSTGRES_DB'),
-            'USER': os.environ.get('POSTGRES_USER'),
-            'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-            'HOST': os.environ.get('POSTGRES_HOST'),
-            'PORT': os.environ.get('POSTGRES_PORT'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-else:  # For localhost development
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #         'NAME': 'asdjadajsnd',
-    #         'USER': 'snkdfjnksjdf',
-    #         'PASSWORD': 'asjndkjnaskd',
-    #         'HOST': 'localhost',
-    #         'PORT': '5432',
-    #     }
-    # }
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
